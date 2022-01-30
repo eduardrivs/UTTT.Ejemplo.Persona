939a37fc0b0fafab5c279c0afc00271e7548c7b7
Eduardo Rivas <eduardrivs@gmail.com>
2022-01-29 23:10:13 -0600

    Solucion de Error Somee Nuevo Servidor de correo

--- UTTT.Ejemplo.Persona/EmailManager.cs
+++ UTTT.Ejemplo.Persona/EmailManager.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Net.Mail;
 using System.Configuration;
-using System.Web.Configuration;
-using System.Net.Configuration;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
+using System.Net.Configuration;
+using System.Net.Mail;
 using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Web;
+using System.Web.Configuration;
 
 namespace UTTT.Ejemplo.Persona
 {
@@ -23,30 +21,31 @@ namespace UTTT.Ejemplo.Persona
             this._eMail = email;
         }
 
-        public void enviarMensaje()
+        public void enviarMensaje(String mensaje)
         {
-            MailMessage correo = new MailMessage();
-            Configuration c = WebConfigurationManager.OpenWebConfiguration(HttpContext.Current.Request.ApplicationPath);
-            MailSettingsSectionGroup settings = (MailSettingsSectionGroup)c.GetSectionGroup("system.net/mailSettings");
-            correo.From = new System.Net.Mail.MailAddress(settings.Smtp.From, settings.Smtp.Network.UserName, System.Text.Encoding.UTF8); //Correo de salida
-            correo.To.Add(_eMail); //Correo destino
-            correo.Subject = "ERROR en aplicacion http://www.EduardoRivas.somee.com"; //Asunto
-            StringBuilder sb = new StringBuilder();
-            sb.Append("ERROR INTERNO: "+DateTime.Now.ToString());
-            sb.Append(Environment.NewLine);
-            sb.Append(PersonaPrincipal._ultimoError);
-            sb.Append(Environment.NewLine);
-            correo.Body = sb.ToString(); //Mensaje del correo
-            correo.IsBodyHtml = true;
-            correo.Priority = MailPriority.Normal;
-            SmtpClient smtp = new SmtpClient();
-            smtp.UseDefaultCredentials = settings.Smtp.Network.DefaultCredentials;
-            smtp.Host = settings.Smtp.Network.Host; //Host del servidor de correo
-            smtp.Port = settings.Smtp.Network.Port; //Puerto de salida
-            smtp.Credentials = new System.Net.NetworkCredential(settings.Smtp.From, settings.Smtp.Network.Password); //Cuenta de correo
-            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
-            smtp.EnableSsl = settings.Smtp.Network.EnableSsl;//True si el servidor de correo permite ssl
-            smtp.Send(correo);
+            var fromAddress = new MailAddress("eduardo.rivas.uttt@gmail.com", "Eduardo Rivas");
+            var toAddress = new MailAddress(_eMail);
+            const string fromPassword = "Uurxi!5b.F8tAYQ";
+            const string subject = "ERROR en sitio http://www.EduardoRivas.somee.com";
+            var body = "Error del "+System.DateTime.Now.ToString()+"\n\n"+mensaje;
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
         }
     }
 }
